08af66e2fb7dd236ade446d82ace700ffe8dd07b
mdhasibseo <mdhasibseoexpet@gmail.com>
2026-05-01 22:13:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from './Component/Banner'
+import CircularSkills from './Component/Skill'
+
+const App = () => {
+  return (
+    <>
+    <Banner/>
+    <CircularSkills />
+   
+    </>
+  )
+}
+
+export default App
